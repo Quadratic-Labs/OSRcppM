9392563f06cc4676bc5f21d7b21ce638370d3d0f
benoitt <benoit.thieurmel@datastorm.fr>
2019-01-25 14:33:28 +0100

    fix DESCRIPTION + update tests

--- test.R
+++ test.R
@@ -8,18 +8,18 @@ data <- data.frame(id = 1:21, lat = seq(50.4, 50.6, by = 0.01), lon = seq(3, 3.2
 
 
 system.time(res1 <- OSRMroute(data[rep(1:20, 100), ], data[rep(1:10, 1), ], 
-                             "/home/benoit/osrm-data/nord-pas-de-calais-latest.osrm", nthreads = 1))
+                             "/home/benoitt/osrm/osrm-data/nord-pas-de-calais-latest.osrm", nthreads = 1))
 
 system.time(res2 <- OSRMroute(data[rep(1:20, 100), ], data[rep(1:10, 1), ], 
-                             "/home/benoit/osrm-data/nord-pas-de-calais-latest.osrm", nthreads = 4))
+                              "/home/benoitt/osrm/osrm-data/nord-pas-de-calais-latest.osrm", nthreads = 4))
 
 system.time(res3 <- OSRMroute(data[rep(1:20, 100), ], data[rep(1:10, 1), ], 
-                             "/home/benoit/osrm-data/nord-pas-de-calais-latest.osrm", nthreads = 8))
+                              "/home/benoitt/osrm/osrm-data/nord-pas-de-calais-latest.osrm", nthreads = 8))
 
 all.equal(res1, res2)
 all.equal(res2, res3)
 
 system.time(res <- OSRMroute(data[rep(1:20, 1000), ], 
                              data[rep(1:10, 10), ], 
-                             "/home/benoit/osrm-data/nord-pas-de-calais-latest.osrm", 
+                             "/home/benoitt/osrm/osrm-data/nord-pas-de-calais-latest.osrm", 
                              nthreads = 8))
